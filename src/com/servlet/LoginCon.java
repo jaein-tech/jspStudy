e04028bec142810da371713985e42cc5ba8d4b18
joojaein <dvpin0102@gmail.com>
2020-03-10 23:22:39 +0900

    session을 이용해서 로그인 로그아웃 연습

--- src/com/servlet/LoginCon.java
+++ src/com/servlet/LoginCon.java
@@ -10,11 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/loginCon")
-public class loginCon extends HttpServlet {
 
+@WebServlet("/loginCon")
+public class LoginCon extends HttpServlet {
+       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		PrintWriter out = response.getWriter();
 		
 		String mID = request.getParameter("mID");
@@ -42,12 +42,11 @@ public class loginCon extends HttpServlet {
 		cookie.setMaxAge(60*60);
 		
 		response.sendRedirect("loginOK.jsp");
-		
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	
 		doGet(request, response);
 	}
 
